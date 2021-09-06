60fdb1bbd8ff669351d1f0f4896e2d9b7aed6890
Rob <guisalmon@gmail.com>
2021-09-06 12:31:19 +0200

    Redraw only necessary chars'

--- clock.py
+++ clock.py
@@ -10,8 +10,8 @@ charset = [
     [0x00, 0x00, 0x00, 0x00, 0x1F, 0x1F, 0x1F, 0X1F],
     [0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F],
     [0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E],
-    [0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03],
-    [0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18]
+    [0x00, 0x00, 0x00, 0x03, 0x03, 0x03, 0x03, 0x03],
+    [0x00, 0x00, 0x00, 0x18, 0x18, 0x18, 0x18, 0x18]
 ]
 
 digitset = [
@@ -40,6 +40,12 @@ digitset_bold = [
     [1, 1, 1, 1, 1, 3, 1, 1, 2, 2, 1, 1, 0, 0, 1, 1]
 ]
 
+h = "00"
+m = "00"
+d = "00"
+p = True
+month = "00"
+y = "0000"
 
 lcd = RPi_I2C_driver.Lcd()
 lcd.lcd_clear()
@@ -80,33 +86,51 @@ def get_backlight():
     return backlight
 
 def clock():
+    global h
+    global m
+    global d
+    global p
+    global month
+    global y
     while 1:
         if backlight == 1:
             now = datetime.now()
-            h = now.strftime("%H")
-            m = now.strftime("%M")
-            p = int(now.strftime("%S")) % 2 == 0
-            d = now.strftime("%d")
-            month = now.strftime("%m")
-            y = now.strftime("%Y")
-            write_number(int(h[0]), 0)
-            write_number(int(h[1]), 1)
-            write_number(int(m[0]), 2)
-            write_number(int(m[1]), 3)
-            lcd.lcd_display_string_pos(d, 1, 0)
-            lcd.lcd_display_string_pos(month, 2, 0)
-            lcd.lcd_display_string_pos(y[0:2], 3, 0)
-            lcd.lcd_display_string_pos(y[2:4], 4, 0)
-            if p:
-                lcd.lcd_display_string_pos(chr(6), 2, 10)
-                lcd.lcd_display_string_pos(chr(7), 2, 11)
-                lcd.lcd_display_string_pos(chr(6), 4, 10)
-                lcd.lcd_display_string_pos(chr(7), 4, 11)
-            else:
-                lcd.lcd_display_string_pos(chr(0), 2, 10)
-                lcd.lcd_display_string_pos(chr(0), 2, 11)
-                lcd.lcd_display_string_pos(chr(0), 4, 10)
-                lcd.lcd_display_string_pos(chr(0), 4, 11)
-            sleep(0.2)
+            ht = now.strftime("%H")
+            mt = now.strftime("%M")
+            pt = int(now.strftime("%S")) % 2 == 0
+            dt = now.strftime("%d")
+            month_t = now.strftime("%m")
+            yt = now.strftime("%Y")
+            if h != ht:
+                write_number(int(ht[0]), 0)
+                write_number(int(ht[1]), 1)
+                h = ht
+            if m != mt:
+                write_number(int(mt[0]), 2)
+                write_number(int(mt[1]), 3)
+                m = mt
+            if d != dt:
+                lcd.lcd_display_string_pos(dt, 1, 0)
+                d = dt
+            if month != month_t:
+                lcd.lcd_display_string_pos(month_t, 2, 0)
+                month = month_t
+            if y != yt:
+                lcd.lcd_display_string_pos(yt[0:2], 3, 0)
+                lcd.lcd_display_string_pos(yt[2:4], 4, 0)
+                y = yt
+            if p != pt:
+                if pt:
+                    lcd.lcd_display_string_pos(chr(6), 2, 10)
+                    lcd.lcd_display_string_pos(chr(7), 2, 11)
+                    lcd.lcd_display_string_pos(chr(6), 4, 10)
+                    lcd.lcd_display_string_pos(chr(7), 4, 11)
+                else:
+                    lcd.lcd_display_string_pos(chr(0), 2, 10)
+                    lcd.lcd_display_string_pos(chr(0), 2, 11)
+                    lcd.lcd_display_string_pos(chr(0), 4, 10)
+                    lcd.lcd_display_string_pos(chr(0), 4, 11)
+                p = pt
+            sleep(0.1)
         else:
             lcd.backlight(0)
