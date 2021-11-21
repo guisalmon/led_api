b16c8e175d2f926027a8ac8f21454f2b4fc09bd9
Rob <guisalmon@gmail.com>
2021-11-21 22:14:28 +0100

    Stuff

--- local_api.py
+++ local_api.py
@@ -14,14 +14,20 @@ BIT1_PIN = 27
 BIT2_PIN = 22   # violet
 BIT3_PIN = 23   # rose
 BIT4_PIN = 24
+POWER_PIN = 16
+SOUND_PIN = 20
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BIT1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BIT2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BIT3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BIT4_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(POWER_PIN, GPIO.OUT)
+GPIO.setup(SOUND_PIN, GPIO.OUT)
 
 STRIP = film_light.init_strip()
+
+on = False
 
 
 class Clock(threading.Thread):
@@ -46,6 +52,22 @@ def toggle_film_light():
     film_light.toggle_light(STRIP)
 
 
+def toggle_sound():
+    GPIO.output(SOUND_PIN, GPIO.LOW)
+    time.sleep(50 / 1000)
+    GPIO.output(SOUND_PIN, GPIO.HIGH)
+
+
+def toggle_power():
+    global on
+    if on:
+        GPIO.output(POWER_PIN, GPIO.HIGH)
+        on = False
+    else:
+        GPIO.output(POWER_PIN, GPIO.LOW)
+        on = True
+
+
 if __name__ == '__main__':
     print("Up and running!")
     clock_thread = Clock(clock.clock)
@@ -53,7 +75,11 @@ if __name__ == '__main__':
 
     GPIO.add_event_detect(BIT1_PIN, GPIO.RISING, callback=lambda x: toggle_clock_backlight(), bouncetime=500)
     GPIO.add_event_detect(BIT2_PIN, GPIO.RISING, callback=lambda x: toggle_film_light(), bouncetime=500)
-    GPIO.add_event_detect(BIT3_PIN, GPIO.RISING, callback=lambda x: toggle(2), bouncetime=500)
-    GPIO.add_event_detect(BIT4_PIN, GPIO.RISING, callback=lambda x: toggle(3), bouncetime=500)
+    GPIO.add_event_detect(BIT3_PIN, GPIO.RISING, callback=lambda x: toggle_power(), bouncetime=500)
+    GPIO.add_event_detect(BIT4_PIN, GPIO.RISING, callback=lambda x: toggle_sound(), bouncetime=500)
+
+    GPIO.output(SOUND_PIN, GPIO.HIGH)
+    GPIO.output(POWER_PIN, GPIO.HIGH)
+
     while True:
         time.sleep(1)
